ebe214eeb398b13910e64d4c07d9dc4b88a418a1
mattiasBillast <Gebruiker@10.43.72.255>
2018-10-28 16:30:46 +0100

    version

--- 1D_Full_Euler.py
+++ 1D_Full_Euler.py
@@ -1,79 +1,150 @@
-
+import scipy
 
 import numpy as np
-import scipy as sc
+from scipy import linalg
+import matplotlib.pyplot as plt
 
-#parameters
-dx = 0.01
-dt = 0.001
+def main():
+    #parameters
+    dx = 0.02
+    dt = 0.00005
 
-#Number of gridpoints
-# nu = 10
-# N = 2**nu
+    #Number of gridpoints
+    # nu = 10
+    # N = 2**nu
 
-test = 1
-if test==1:
-    x_l = -0.5
-    x_r = 0.5
-    # create mesh
-    grid = np.arange(x_l - dx / 2, x_r + dx, dx)
-    interior_points = range(1, np.shape(grid)[0] - 1)
-    n_grid = np.shape(grid)[0]
-    #initial conditions primitive variables
-    gamma = 1.4
-    V = np.zeros((1,n_grid))
-    P = 8/gamma*np.ones((1,n_grid/2))
-    P = np.append(P,np.ones((1,n_grid/2)))
-    Rho = np.ones((1, n_grid/2))*8
-    Rho = np.append(Rho,np.ones((1, n_grid/2)))
-    t_max =0.2
+    test = 1
+    if test==1:
+        x_l = -0.5
+        x_r = 0.5
+        # create mesh
+        grid = np.arange(x_l - dx / 2, x_r + dx, dx)
+        interior_points = range(1, np.shape(grid)[0] - 1)
+        n_grid = np.shape(grid)[0]
+        #initial conditions primitive variables
+        gamma = 1.4
+        V = np.zeros((1,n_grid))
+        P = 8/gamma*np.ones((1,n_grid/2))
+        P = np.append(P,np.ones((1,n_grid/2)))
+        Rho = np.ones((1, n_grid/2))*8
+        Rho = np.append(Rho,np.ones((1, n_grid/2)))
+        t_max =0.02
 
-elif test==2:
-    # gamma = 1.4
-    # v = 0.0
-    # p = 8.0 / gamma
-    # rho = 8.0
-    x_l = 0.0
-    x_r = 1.0
-    t_max = 3
-s=(3,3)
-# A = np.zeros(s)
-# #conservative variables U
-# U = np.array([rho,v*rho,p/(gamma-1)+rho*v**2/2.0])
-# A = np.matrix([[0,1,0],[(gamma-3)/2.0*(U[1]/U[0])**2,(3-gamma)*U[1]/U[0],gamma-1],[-gamma*U[1]*U[2]/U[0]**2+(gamma-1)*(U[1]/U[0])**3,gamma*U[2]/U[0]-3/2*(gamma-1)*(U[1]/U[0])**2,gamma*U[1]/U[0]]])
-#
-# w,r=np.linalg.eig(A)
-# print(w)
-
-# #primitive variables V
-# U = np.array([rho,v,p])
-# A_p = np.matrix([[v,rho,0],[0,v,1.0/rho],[0,gamma*p,v]])
-# w_l,r_l=np.linalg.eig(A_p)
-# print(r_l)
-
-
-
-#array with solutions
-values = np.zeros((3,np.shape(grid)[0],1))
-values[0,:,0]=Rho
-values[1,:,0]=V
-values[2,:,0]=P
-print(values)
-#print(interior_points)
-#values on grid n(dt) x m (dx)
-t=0
-while t<t_max:
-    t+=dt
-    #make new timestep in matrix
-    #set boundary conditions
-    #change interior points
+    elif test==2:
+         gamma = 1.4
+        # v = 0.0
+        # p = 8.0 / gamma
+        # rho = 8.0
+        x_l = 0.0
+        x_r = 1.0
+        # create mesh
+        grid = np.arange(x_l - dx / 2, x_r + dx, dx)
+        interior_points = range(1, np.shape(grid)[0] - 1)
+        n_grid = np.shape(grid)[0]
+        V = np.zeros((1, n_grid))
+        P = 0.1+0.0001*np.exp(-(grid-0.5)**2/0.1**2)
+        Rho = gamma*P
+        t_max = 3
+    s=(3,3)
+    # A = np.zeros(s)
+    # #conservative variables U
+    # U = np.array([rho,v*rho,p/(gamma-1)+rho*v**2/2.0])
+    # A = np.matrix([[0,1,0],[(gamma-3)/2.0*(U[1]/U[0])**2,(3-gamma)*U[1]/U[0],gamma-1],[-gamma*U[1]*U[2]/U[0]**2+(gamma-1)*(U[1]/U[0])**3,gamma*U[2]/U[0]-3/2*(gamma-1)*(U[1]/U[0])**2,gamma*U[1]/U[0]]])
     #
+    # w,r=np.linalg.eig(A)
+    # print(w)
+
+    # #primitive variables V
+    # U = np.array([rho,v,p])
+    # A_p = np.matrix([[v,rho,0],[0,v,1.0/rho],[0,gamma*p,v]])
+    # w_l,r_l=np.linalg.eig(A_p)
+    # print(r_l)
 
 
-def compute_eigenvalues(U,gamma):
-    rho=U[0]
-    v = U[1]
-    p = U[2]
-    A = np.matrix([[v,rho,0],[0,v,1.0/rho],[0,gamma*p,v]])
-    e, R = np.linalg.eig(A)
-    return {'e':e,'R':R}
+
+    #array with solutions
+    values = np.zeros((3,np.shape(grid)[0],1))
+    values[0,:,0]=Rho
+    values[1,:,0]=V
+    values[2,:,0]=P
+    #print(values)
+    #print(interior_points)
+    #values on grid n(dt) x m (dx)
+    t=0
+
+
+
+
+    while t<t_max:
+        #make new timestep in matrix
+        #set boundary conditions
+        #change interior points
+        Rs = np.array([[[]]])
+        for i in xrange(0,n_grid):
+            decom = compute_eigenvalues(values[:,i,-1],gamma)
+            diagsi = convert_to_diag(values[:,i,-1],decom['L'])
+
+            if (i==0):
+                Rs = np.array([decom['R']])
+
+                diag = np.matrix([diagsi]).T
+            else:
+                Rs = np.append(Rs, np.array([decom['R']]), axis=0)
+                diag=np.append(diag,np.matrix([diagsi]).T,axis=1)
+            if (i == n_grid-1):
+                z = diag[:,1:-1] -get_flux(decom['e'], diag, dx, dt)[:,0:-1]
+        t += dt
+        for i in xrange(1,n_grid-1):
+            if(i==1):
+                prim = convert_to_prim(z[:,i-1],Rs[i,:,:])
+            else:
+                prim = np.append(prim,convert_to_prim(z[:,i-1],Rs[i,:,:]),axis=1)
+        values = np.append(values,np.zeros((3,n_grid,1)),axis=2)
+        #keep boundary conditions
+        values[:,0,-1] = values[:,0,-2]
+        values[:, -1, -1] = values[:, -1, -2]
+        values[:,1:-1,-1] = prim
+        #print(decom['R']* decom['L'])
+    #values.tofile('test1.dat')
+    plt.figure(1)
+    plt.plot(values[0, :, -1])
+    plt.ylabel('rho')
+
+    plt.figure(2)
+    plt.plot(values[1, :, -1])
+    plt.ylabel('v')
+    plt.figure(3)
+    plt.plot(values[2, :, -1])
+    plt.ylabel('p')
+    plt.show()
+    plt.close()
+
+def get_flux(_e, _values, _dx, _dt):
+    f = np.multiply(np.matrix([_e]).T,(_values[:,1:]-_values[:,:-1])*_dt/_dx)
+    return f
+
+
+def compute_eigenvalues(_U,_gamma):
+
+    rho=_U[0]
+    v = _U[1]
+    p = _U[2]
+    A = np.array([[v,rho,0.],[0.,v,1.0/rho],[0.,_gamma*p,v]])
+
+    e,R = np.linalg.eig(A)
+    L = np.linalg.inv(R)
+    return {'e':e,'L':L,'R':R}
+
+
+def convert_to_diag(_values,_L):
+    diags = np.matmul(_L,_values)
+    return diags
+
+
+def convert_to_prim(_diags,_R):
+    prim = np.matmul(_R,_diags)
+    return prim
+
+
+main()
+
