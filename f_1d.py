c5931ea9df09e89b5e6fd6b9e9390fb785419860
mattiasBillast <Gebruiker@10.43.72.255>
2018-10-30 08:46:29 +0100

    version

--- f_1d.py
+++ f_1d.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
-
+#https://github.com/ibackus/sod-shocktube
+import sod
 import numpy as np
 from scipy import linalg
 import matplotlib.pyplot as plt
@@ -7,14 +8,14 @@ import matplotlib.pyplot as plt
 
 def main():
     #parameters
-    dx = 0.02
+    dx = 0.01
     dt = 0.001
 
     #Number of gridpoints
     # nu = 10
     # N = 2**nu
 
-    test = 2
+    test = 1
     if test==1:
         x_l = -0.5
         x_r = 0.5
@@ -43,7 +44,7 @@ def main():
         interior_points = range(1, np.shape(grid)[0] - 1)
         n_grid = np.shape(grid)[0]
         V = np.zeros((1, n_grid))
-        P = 0.1 + 0.0001 * np.exp(-(grid - 0.5) ** 2 / 0.1 ** 2)
+        P = 0.1 + 0.001 * np.exp(-(grid - 0.5) ** 2 / 0.1 ** 2)
         Rho = gamma * P
         t_max = 3
     s=(3,3)
@@ -83,21 +84,61 @@ def main():
         Rs = np.array([[[]]])
         values = np.append(values, np.zeros((3, n_grid, 1)), axis=2)
 
-        for i in xrange(1,n_grid-1):
-            decom = compute_eigenvalues(values[:,i,-2],gamma)
-            diagsi = convert_to_diag(values[:,i,-2],decom['L'])
-            #if (i>0):
-            diagsimin = convert_to_diag(values[:,i-1,-2],decom['L'])
-            diagsiplus = convert_to_diag(values[:,i+1,-2],decom['L'])
-            verschil = np.zeros((3,1))
-            for j in xrange(0,3):
-                if (decom['e'][j]>=0):
-                    verschil[j][0] = diagsi[j]-diagsimin[j]
-                elif (decom['e'][j]<0):
-                    verschil[j][0] = diagsiplus[j]-diagsi[j]
-            z = np.array([np.matmul(decom['L'],values[:,i,-2])]).T-dt/(2*dx)*np.multiply(np.array([decom['e']]).T,verschil)
-            #print(decom['R'])
-            values[:,i,-1] = np.matmul(decom['R'],z[:,0])
+        for i in xrange(0,n_grid):
+            if (i==0):
+                if test==2:
+                    decom = compute_eigenvalues(values[:, i, -2], gamma)
+                    diagsi = convert_to_diag(values[:, i, -2], decom['L'])
+                    # if (i>0):
+                    diagsimin = convert_to_diag(values[:, i - 1, -2], decom['L'])
+                    diagsiplus = convert_to_diag(values[:, i + 1, -2], decom['L'])
+
+                    verschil = np.zeros((3, 1))
+                    for j in xrange(0, 3):
+                        if (decom['e'][j] >= 0):
+                            verschil[j][0] = diagsi[j] - diagsimin[j]
+                        elif (decom['e'][j] < 0):
+                            verschil[j][0] = diagsiplus[j] - diagsi[j]
+                    z = np.array([np.matmul(decom['L'], values[:, i, -2])]).T - dt / (dx) * np.multiply(
+                        np.array([decom['e']]).T, verschil)
+                    # print(decom['R'])
+                    values[:, i, -1] = np.matmul(decom['R'], z[:, 0])
+
+            elif(i==n_grid-1):
+                if test == 2:
+                    decom = compute_eigenvalues(values[:, i, -2], gamma)
+                    diagsi = convert_to_diag(values[:, i, -2], decom['L'])
+                    # if (i>0):
+                    diagsimin = convert_to_diag(values[:, i - 1, -2], decom['L'])
+                    diagsiplus = convert_to_diag(values[:, 0, -2], decom['L'])
+
+                    verschil = np.zeros((3, 1))
+                    for j in xrange(0, 3):
+                        if (decom['e'][j] >= 0):
+                            verschil[j][0] = diagsi[j] - diagsimin[j]
+                        elif (decom['e'][j] < 0):
+                            verschil[j][0] = diagsiplus[j] - diagsi[j]
+                    z = np.array([np.matmul(decom['L'], values[:, i, -2])]).T - dt / (dx) * np.multiply(
+                        np.array([decom['e']]).T, verschil)
+                    # print(decom['R'])
+                    values[:, i, -1] = np.matmul(decom['R'], z[:, 0])
+
+            else:
+                decom = compute_eigenvalues(values[:,i,-2],gamma)
+                diagsi = convert_to_diag(values[:,i,-2],decom['L'])
+                #if (i>0):
+                diagsimin = convert_to_diag(values[:,i-1,-2],decom['L'])
+                diagsiplus = convert_to_diag(values[:,i+1,-2],decom['L'])
+
+                verschil = np.zeros((3,1))
+                for j in xrange(0,3):
+                    if (decom['e'][j]>=0):
+                        verschil[j][0] = diagsi[j]-diagsimin[j]
+                    elif (decom['e'][j]<0):
+                        verschil[j][0] = diagsiplus[j]-diagsi[j]
+                z = np.array([np.matmul(decom['L'],values[:,i,-2])]).T-dt/(dx)*np.multiply(np.array([decom['e']]).T,verschil)
+                #print(decom['R'])
+                values[:,i,-1] = np.matmul(decom['R'],z[:,0])
             # if (i==0):
             #     Rs = np.array([decom['R']])
             #
@@ -107,8 +148,10 @@ def main():
             #     diag=np.append(diag,np.matrix([diagsi]).T,axis=1)
             # if (i == n_grid-1):
             #     z = diag[:,1:-1] -get_flux(decom['e'], diag, dx, dt)[:,0:-1]
-        values[:, 0, -1] = values[:, 1, -1]
-        values[:, -1, -1] = values[:, -2, -1]
+        if test == 1:
+            values[:, 0, -1] = values[:, 1, -1]
+            values[:, -1, -1] = values[:, -2, -1]
+
         t += dt
         print(t/t_max,'%',end='\r')
         # for i in xrange(1,n_grid-1):
@@ -121,17 +164,45 @@ def main():
         #
         # values[:,1:-1,-1] = prim
         #print(decom['R']* decom['L'])
-    values.tofile('test2x02t001.dat')
+    #values.tofile('test2x00t001.dat')
     plt.figure(1)
-    plt.plot(values[0,:,-1])
-    plt.ylabel('rho')
+    plt.plot(grid,values[0,:,-1])
+    plt.ylabel('density')
+    plt.xlabel('x')
 
     plt.figure(2)
-    plt.plot(values[1, :, -1])
-    plt.ylabel('v')
+    plt.plot(grid,values[1, :, -1])
+    plt.ylabel('velocity')
+    plt.xlabel('x')
+
     plt.figure(3)
-    plt.plot(values[2, :, -1])
-    plt.ylabel('p')
+    plt.plot(grid,values[2, :, -1])
+    plt.ylabel('pressure')
+    plt.xlabel('x')
+
+    if test==1:
+        analytic = analytic_solution(gamma,n_grid)
+        p = analytic['p']
+        rho = analytic['rho']
+        u = analytic['u']
+        x_analytic = analytic['x']
+        plt.figure(1)
+        plt.plot(x_analytic, rho, color='g')
+        plt.ylabel('pressure')
+        plt.xlabel('x')
+        #plt.axis([0, 1, 0, 1.1])
+
+        plt.figure(2)
+        plt.plot(x_analytic, u, color='g')
+        plt.ylabel('density')
+        plt.xlabel('x')
+        # plt.axis([0, 1, 0, 1.1])
+
+        plt.figure(3)
+        plt.plot(x_analytic,p, color='g')
+        plt.ylabel('velocity')
+        plt.xlabel('x')
+
     plt.show()
     plt.close()
 
@@ -161,6 +232,11 @@ def convert_to_prim(_diags,_R):
     prim = np.matmul(_R,_diags)
     return prim
 
+def analytic_solution(gamma, npts):
+    positions, regions, values = sod.solve(left_state=(8./gamma, 8, 0), right_state=(1, 1, 0.),
+                                           geometry=(-0.5, 0.5, 0.), t=0.2, gamma=gamma, npts=npts)
+    return values
+#abs(max([3, 7, -10], key=abs)) EOC
 
 main()
 
